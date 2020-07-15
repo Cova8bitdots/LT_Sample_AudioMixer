108f076b70f34c6cc23623114cd66430fb3a7be1
Cova8bitdot <cova.tech.8bitdots@gmail.com>
2020-07-15 16:55:23 +0900

    Gimmick 部分作成

--- Assets/002_Scripts/Gimmick/AutomaticDoor.cs
+++ Assets/002_Scripts/Gimmick/AutomaticDoor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 using CovaTech.UnitySound;
+using Cysharp.Threading.Tasks;
 
 namespace CovaTech.LT.AudioMixerSample
 {
@@ -12,8 +13,19 @@ namespace CovaTech.LT.AudioMixerSample
         // メンバ変数関連
         //------------------------------------------------------------------
         #region  ===== MEMBER_VARIABLES =====
-        
+        [SerializeField]
+        private Transform m_doorObject =null;
+        [SerializeField, Range( 0.01f, 10.0f)]
+        private float m_animTime =1.0f;
+        [SerializeField, Range( -360.0f, 360.0f)]
+        private float m_rotAngle =0.0f;
+
+        private IAudioPlayer m_audioPlayer = null;
         private IMixerEffectController m_effectCtrl = null;
+
+        private Coroutine m_doorAnimCoroutine = null;
+
+        private int m_bgmHandler = SoundConsts.INVALID_HANDLER;
         #endregion //) ===== MEMBER_VARIABLES =====
         
 
@@ -23,22 +35,51 @@ namespace CovaTech.LT.AudioMixerSample
         #region ===== INITIALIZE =====
 
         [Inject]
-        public void Initialize( IMixerEffectController _mixerEffectCtrl )
+        public void Initialize( IAudioPlayer _audioPlayer, IMixerEffectController _mixerEffectCtrl )
         {
+            Debug.Assert( _audioPlayer != null);
             Debug.Assert( _mixerEffectCtrl != null);
+            m_audioPlayer = _audioPlayer;
             m_effectCtrl = _mixerEffectCtrl;
         }
 
+        private async void Start()
+        {
+            m_effectCtrl?.SetLowPassFilter( SOUND_CATEGORY.DIEGETIC_BGM, 0.0f);
+            if( m_audioPlayer != null )
+            {
+                m_bgmHandler =  await m_audioPlayer.PlayBgm( (int)BGM_ID.SAMPLE_BGM, 1.0f, true, new System.Threading.CancellationToken() );
+            }
+        }
         #endregion //) ===== INITIALIZE =====
 
-
-        private void OnCollisionEnter()
+        private void OnDestroy()
         {
-
+            if( m_audioPlayer != null && m_bgmHandler != SoundConsts.INVALID_HANDLER )
+            {
+                m_audioPlayer.StopBGM( m_bgmHandler, true, new System.Threading.CancellationToken() ).Forget( e => Debug.LogError(e.Message));
+            }
         }
 
-        private void OnCollisionExit()
+
+        private void OnTriggerEnter()
         {
+            if( m_doorAnimCoroutine != null )
+            {
+                StopCoroutine(m_doorAnimCoroutine);
+                m_doorAnimCoroutine = null;
+            }
+            m_doorAnimCoroutine = StartCoroutine( OpenDoorAnimation() );
+        }
+
+        private void OnTriggerExit()
+        {
+            if( m_doorAnimCoroutine != null )
+            {
+                StopCoroutine(m_doorAnimCoroutine);
+                m_doorAnimCoroutine = null;
+            }
+            m_doorAnimCoroutine = StartCoroutine( CloseDoorAnimation() );
             
         }
 
@@ -47,12 +88,48 @@ namespace CovaTech.LT.AudioMixerSample
 
         private IEnumerator OpenDoorAnimation()
         {
+            if( m_doorObject == null )
+            {
+                yield break;
+            }
+
+            Quaternion currentRotation = m_doorObject.localRotation;
+            Quaternion targetRotation = Quaternion.Euler( m_rotAngle * Vector3.up);
+            for (float t = 0; t < m_animTime; t+= Time.deltaTime)
+            {
+                float ratio = Mathf.Clamp01(t / m_animTime);
+                m_doorObject.localRotation = Quaternion.Lerp( currentRotation, targetRotation, ratio);
+                m_effectCtrl?.SetLowPassFilter( SOUND_CATEGORY.DIEGETIC_BGM, ratio);
+                yield return null;
+            }
+            m_doorObject.localRotation = targetRotation;
+            m_effectCtrl?.SetLowPassFilter( SOUND_CATEGORY.DIEGETIC_BGM, 1.0f);
+
+
             yield return null;
         }
 
         private IEnumerator CloseDoorAnimation()
         {
-            yield return null;
+            if( m_doorObject == null )
+            {
+                yield break;
+            }
+
+            Quaternion currentRotation = m_doorObject.localRotation;
+            Quaternion targetRotation = Quaternion.Euler( Vector3.zero);
+            for (float t = 0; t < m_animTime; t+= Time.deltaTime)
+            {
+                float ratio = Mathf.Clamp01(t / m_animTime);
+                m_doorObject.localRotation = Quaternion.Lerp( currentRotation, targetRotation, ratio);
+                m_effectCtrl?.SetLowPassFilter( SOUND_CATEGORY.DIEGETIC_BGM, 1.0f - ratio);
+                yield return null;
+            }
+            m_doorObject.localRotation = targetRotation;
+
+            m_doorObject.localRotation = Quaternion.Euler( Vector3.zero);
+            m_effectCtrl?.SetLowPassFilter( SOUND_CATEGORY.DIEGETIC_BGM, 0.0f);
+
         }
     }
 }
---
+++ Assets/002_Scripts/Sound/BgmId.cs
@@ -0,0 +1,15 @@
+﻿namespace CovaTech.LT.AudioMixerSample
+{
+    public enum BGM_ID
+    {
+        UNDEFINED = -1,
+
+        CATEGORY_BGM_SYSTEM = 10000,
+
+
+
+        CATEGORY_VFM_DIEGETIC= 20000,
+        SAMPLE_BGM,
+
+    }
+}
